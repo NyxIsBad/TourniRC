b3c768976acc5b8c7fa35082a50f16495b2f3481
NyxIsBad <hijirijst@gmail.com>
2024-07-31 19:03:21 -0400

    chat experimentation

--- ui.py
+++ ui.py
@@ -7,12 +7,19 @@ from typing import *
 app = Flask(__name__)
 config = uiConfig()
 
+# ---------------------
+# Main Routes
+# ---------------------
 @app.route('/')
 def chat():
     cur_theme = config.get_theme()
     print(cur_theme)
     return render_template('chat.html', cur_theme=cur_theme, themes=THEMES)
 
+
+# ---------------------
+# API Routes
+# ---------------------
 @app.route('/set_theme', methods=['POST'])
 def set_theme():
     data: Dict[str, Any] = request.get_json()
@@ -27,9 +34,9 @@ def set_theme():
         return jsonify(success=True), 200
     return jsonify(success=False), 400
 
-@app.route('/login')
-def example():
-    return render_template('login.html')
+# ---------------------
+# Starting webserver
+# ---------------------
 
 def debug_run():
     app.run()
@@ -37,5 +44,8 @@ def debug_run():
 def prod_run():
     serve(app=app, host="0.0.0.0", port=5000)
 
+# ---------------------
+# Used for debug since we will call from main normally
+# ---------------------
 if __name__ == "__main__":
     debug_run()
