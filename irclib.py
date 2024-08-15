87479bb45a04ffbf535b2303364d345d099d31fc
NyxIsBad <hijirijst@gmail.com>
2024-08-15 13:04:34 -0400

    some changes

--- irclib.py
+++ irclib.py
@@ -12,6 +12,9 @@ class Client(osu_irc.Client):
         self.msgs = {}
         self.chats = {}
         self.sio = sioClient()
+
+        self.sio.on('bounce_send_msg', self.onBounceSendMessage)
+        self.sio.on('bounce_chat_removed', self.onBounceChatRemoved)
         self.sio.connect('http://127.0.0.1:5000')
 
     async def onReady(self):
@@ -20,39 +23,11 @@ class Client(osu_irc.Client):
         self.logger.info(f"Connected to server")
 
     async def onMessage(self, message: osu_irc.Message):
-        if message.room_name not in self.chats:
-            self.chats[message.room_name] = Chat(message.room_name, message.channel_type)
-        self.chats[message.room_name].add_message(message)
         self.sio.emit('recv_msg', message.compact())
         print(f"Message from {message.user_name}: {message.content}")
-
-class Chat():
-    def __init__(self, channel_name: str, channel_type: int):
-        self.type = channel_type
-        self.channel_name = channel_name
-        self.messages: List[osu_irc.Message] = []
-        self.timer = 120 # default to 2 minutes
-        self.start_timer = 5 # default to 5 seconds
-        
-    def add_message(self, message: osu_irc.Message) -> None:
-        if message.room_name == self.channel_name:
-            self.messages.append(message)
-        else:
-            raise ValueError(f"Message not in channel {self.channel_name}")
-        
-    def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} channel_name='{self.channel_name}' num_messages={len(self.messages)}>"
-
-    def __str__(self) -> str:
-        return f"Chat on channel {self.channel_name} with {len(self.messages)} messages."
-
-    @property 
-    def message_count(self) -> int:
-        return len(self.messages)
     
-    @property
-    def get_messages(self) -> List[osu_irc.Message]:
-        return self.messages
-    
-    def clear_messages(self):
-        self.messages.clear()
+    def onBounceSendMessage(self, data: Dict[str, Any]):
+        print(f"Message bounced: {data}")
+
+    def onBounceChatRemoved(self, data: Dict[str, Any]):
+        print(f"Chat removed: {data}")
--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_socketio import SocketIO, emit, join_room, leave_room, send, close_room, rooms, disconnect
+from flask_socketio import SocketIO, emit, send, disconnect
 from cfg import uiConfig, THEMES
 
 from typing import *
@@ -10,6 +10,84 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 config = uiConfig()
 
+#---------------------
+# Chat Classes
+#---------------------
+class Chat():
+    def __init__(self, channel_name: str, channel_type: int):
+        self.type = channel_type
+        self.channel_name = channel_name
+        self.messages: List[List] = []
+        self.timer = 120 # default to 2 minutes
+        self.start_timer = 5 # default to 5 seconds
+        
+    def add_message(self, message: Dict[str, Any]) -> None:
+        if message['room_name'] == self.channel_name:
+            self.messages.append([message['time_recv'], message['user_name'], message['content']])
+        else:
+            raise ValueError(f"Message not in channel {self.channel_name}")
+        
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} channel_name='{self.channel_name}' num_messages={len(self.messages)}>"
+
+    def __str__(self) -> str:
+        return f"Chat on channel {self.channel_name} with {len(self.messages)} messages."
+
+    @property 
+    def message_count(self) -> int:
+        return len(self.messages)
+    
+    def clear_messages(self):
+        self.messages.clear()
+
+class Chats():
+    def __init__(self):
+        self.chats: Dict[str, Chat] = {}
+    
+    def add_chat(self, channel_name: str, channel_type: int) -> None:
+        self.chats[channel_name] = Chat(channel_name, channel_type)
+
+    def remove_chat(self, channel_name: str) -> None:
+        if channel_name in self.chats:
+            self.chats.pop(channel_name)
+            socketio.emit('bounce_chat_removed', {
+                'name': channel_name
+            })
+        else:
+            raise ValueError(f"Channel {channel_name} not found.")
+
+    def get_chat(self, channel_name: str) -> Chat:
+        return self.chats.get(channel_name, None)
+    
+    def add_message(self, message: Dict[str, Any]) -> None:
+        if message['room_name'] in self.chats:
+            self.chats[message['room_name']].add_message(message)
+        else:
+            self.add_chat(message['room_name'], message['channel_type'])
+            self.chats[message['room_name']].add_message(message)
+    
+    def get_messages(self, channel_name: str) -> List[List]:
+        return self.chats[channel_name].messages
+
+    @property 
+    def chat_count(self) -> int:
+        return len(self.chats)
+    
+    @property 
+    def message_count(self) -> int:
+        return sum([chat.message_count for chat in self.chats.values()])
+    
+    @property 
+    def channel_names(self) -> List[str]:
+        return list(self.chats.keys())
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} num_chats={len(self.chats)}>"
+    
+    def __str__(self) -> str:
+        return f"Chats with {len(self.chats)} channels."
+
+chats = Chats()
 # ---------------------
 # Main Routes
 # ---------------------
@@ -49,20 +127,30 @@ def handle_disconnect():
 
 @socketio.on('send_msg')
 def handle_send_msg(data: Dict[str, Any]):
-    print(f"Message sent: {data.get('message', None).strip()}")
+    # print(f"Message sent: {data.get('message', None).strip()}")
+    emit('bounce_send_msg', {
+        'content': data.get('message', None).strip()
+    }, broadcast=True)
 
 @socketio.on('recv_msg')
 def handle_recv_msg(data: Dict[str, Any]):
-    print(f"Message received: {data}")
-    emit('bounce_msg', {
+    chats.add_message(data)
+    emit('bounce_recv_msg', {
         'time': data["time_recv"]*1000, # convert to ms
         'user': data["user_name"],
         'content': data["content"]
     }, broadcast=True)
 
+@socketio.on('tab_swap')
+def handle_tab_swap(data: Dict[str, Any]):
+    print(f"Tab swap: {data['tab']}")
+
 # ---------------------
 # Starting webserver
 # ---------------------
+def start():
+    debug_run()
+
 def debug_run():
     socketio.run(app, debug=True, host='localhost', port=5000)
 
