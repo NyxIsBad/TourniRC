0872b82d6a1ed26930c7b16e139294b51f7dfd5a
NyxIsBad <hijirijst@gmail.com>
2026-08-07 23:00:50 -0400

    new release, fix old security vulnerabilities

--- build-windows.ps1
+++ build-windows.ps1
@@ -7,7 +7,6 @@ try {
     npm.cmd ci
     npm.cmd run build
     python -m PyInstaller --noconfirm --clean TourniRC.spec
-    Copy-Item PORTABLE_README.txt dist\TourniRC\README.txt
     Compress-Archive -Path dist\TourniRC -DestinationPath "dist\TourniRC-$Version-windows-x64.zip" -Force
     Write-Host "Portable build and ZIP created under dist\"
 } finally {
